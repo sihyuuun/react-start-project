e7a4f823b1fdd4cad61a06acc177f900a92ffd72
sihyun <leesh3597@gmail.com>
2022-08-25 21:01:41 +0900

    CoinTracker

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [...currentArray, toDo]);
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(1);
+  const onChange = (event) => {
+    setCost(event.target.value);
+    setNeed(1);
   };
-  const deleteBtn = (index) => {
-    setToDos((currentToDos) =>
-      currentToDos.filter((_, currentIndex) => currentIndex !== index)
-    );
+  const handleInput = (event) => {
+    setNeed(event.target.value);
   };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onChange}>
+          <option>Select Coin!</option>
+          {coins.map((coin, index) => (
+            <option
+              key={index}
+              value={coin.quotes.USD.price}
+              id={coin.symbol}
+              symbol={coin.symbol}>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </option>
+          ))}
+          ;
+        </select>
+      )}
+      <h2>Please enter the amount</h2>
+      <div>
         <input
-          onChange={onChange}
-          value={toDo}
-          type='text'
-          placeholder='Write your to do...'
+          type='number'
+          value={need}
+          onChange={handleInput}
+          placeholder='Dollar'
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <button onClick={() => deleteBtn(index)}>X</button>
-          </li>
-        ))}
-      </ul>
+        $
+      </div>
+      <h2>You can get {need / cost}</h2>
     </div>
   );
 }
