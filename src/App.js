adb155eb6301e8f0fffd7de0472de02154c6d900
sihyun <leesh3597@gmail.com>
2022-08-25 23:57:52 +0900

    MovieApp

--- src/App.js
+++ src/App.js
@@ -1,57 +1,5 @@
-import { useState, useEffect } from "react";
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setNeed(1);
-  };
-  const handleInput = (event) => {
-    setNeed(event.target.value);
-  };
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select onChange={onChange}>
-          <option>Select Coin!</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-          ;
-        </select>
-      )}
-      <h2>Please enter the amount</h2>
-      <div>
-        <input
-          type='number'
-          value={need}
-          onChange={handleInput}
-          placeholder='Dollar'
-        />
-        $
-      </div>
-      <h2>You can get {need / cost}</h2>
-    </div>
-  );
+  return null;
 }
 
 export default App;
