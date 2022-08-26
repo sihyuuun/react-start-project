6165cdc2da9ed203238a47f60324179e5985750c
sihyun <leesh3597@gmail.com>
2022-08-26 13:35:46 +0900

    Parameters

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/hello'>
+        <Route path='/about'>
           <h1>Hello</h1>
         </Route>
-        <Route path='/movie'>
+        <Route path='/movie/:id'>
           <Detail />
         </Route>
         <Route path='/'>
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,37 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  });
+  console.log(movie);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          coverImg={movie.medium_cover_image}
+          title={movie.title}
+          summary={movie.summary}
+          genres={movie.genres}
+        />
+      )}
+    </div>
+  );
 }
 export default Detail;
